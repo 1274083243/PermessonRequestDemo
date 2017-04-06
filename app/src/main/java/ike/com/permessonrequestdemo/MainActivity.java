59e417b9663ee7e79bd04ca7123c91f538ef0b5b
ike <1274083243@qq.com>
2017-04-06 13:28:18 +0800

    android6.0运行时权限demo

---
+++ app/src/main/java/ike/com/permessonrequestdemo/MainActivity.java
@@ -0,0 +1,157 @@
+package ike.com.permessonrequestdemo;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btn_take_pic;
+    private Button btn_read_sd;
+    private static final int REQUEST_CODE_PIC=0;
+    private static final String Tag="MainActivity";
+    private List<String> denyPermission=new ArrayList<>();//被拒绝的权限集合
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        btn_take_pic= (Button) findViewById(R.id.btn_take_pic);
+        btn_read_sd= (Button) findViewById(R.id.btn_read_sd);
+        btn_read_sd.setOnClickListener(this);
+        btn_take_pic.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_read_sd:
+
+                break;
+            case R.id.btn_take_pic:
+                checkAndRequestPermission();
+                break;
+        }
+    }
+
+    private void checkAndRequestPermission() {
+        String[] checkHasPermission = checkHasPermission(Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.CALL_PHONE);
+        if (checkHasPermission.length>0){
+            ActivityCompat.requestPermissions(this,checkHasPermission,REQUEST_CODE_PIC);
+        }
+    }
+
+    /**
+     * 检查是否有该权限
+     * @param permessions
+     */
+    private String[] checkHasPermission(String... permessions){
+         List<String> denyPermissions=new ArrayList<>();//没有被授权的权限集合
+        for (String permission:permessions) {
+            //未获得权限
+            if (ContextCompat.checkSelfPermission(this, permission)!= PackageManager.PERMISSION_GRANTED){
+                denyPermissions.add(permission);
+            }
+        }
+        String[] neesRequestPermission=new String[]{};
+        if (!denyPermissions.isEmpty()){
+            neesRequestPermission= denyPermissions.toArray(neesRequestPermission);
+        }
+        return neesRequestPermission;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_CODE_PIC:
+                if (grantResults.length>0){
+                    for (int i=0;i<grantResults.length;i++){
+                        if (grantResults[i]!=PackageManager.PERMISSION_GRANTED){
+                            denyPermission.add(permissions[i]);
+                        }
+                    }
+                }
+                if (denyPermission.isEmpty()){
+                    Toast.makeText(MainActivity.this,"拍照授权成功",Toast.LENGTH_SHORT).show();
+                }else {
+                    showNoticeDialog();
+
+                }
+                break;
+        }
+
+    }
+    AlertDialog alertDialog = null;
+    boolean isAwaysDeny=false;
+    /**
+     * 显示提醒的对话框
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void showNoticeDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        StringBuilder strBuilder=new StringBuilder();
+        strBuilder.append("亲，我们需要你权限哦:");
+
+        for (String str:denyPermission){
+            strBuilder.append(str+"、");
+            if (!shouldShowRequestPermissionRationale(str)){
+                Log.e(Tag,"是否永远拒绝:"+(!shouldShowRequestPermissionRationale(str)));
+                isAwaysDeny=true;
+            }
+        }
+        String substring = strBuilder.substring(0,strBuilder.toString().lastIndexOf("、") );
+        builder.setTitle("你未授予我们足够的权限哦")
+                .setMessage(substring)
+                .setPositiveButton("授予", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.e(Tag,"isAwaysDeny:"+isAwaysDeny);
+                        if (isAwaysDeny){//表示用户勾选了权限的不再提醒，此时授权应当开启设置界面进行授权
+                            isAwaysDeny=false;
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            Uri uri = Uri.fromParts("package", MainActivity.this.getPackageName(), null);
+                            intent.setData(uri);
+                            MainActivity.this.startActivityForResult(intent, 1);
+
+                        }else {
+                            checkAndRequestPermission();
+                        }
+
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        alertDialog.dismiss();
+                    }
+                });
+        alertDialog = builder.create();
+        alertDialog.show();
+        denyPermission.clear();
+
+    }
+}
